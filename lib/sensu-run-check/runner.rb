41ac19366b6c6c66e49fe554e3f55ae1101e1fa6
Rickard von Essen <rickard.von.essen@gmail.com>
2015-04-17 17:31:15 +0200

    Changed how checks are executed to better handle errors.

--- lib/sensu-run-check/runner.rb
+++ lib/sensu-run-check/runner.rb
@@ -1,5 +1,6 @@
 require "rubygems"
 
+require "open3"
 require "sensu-run-check/version"
 require "sensu/client/process"
 require "sensu/settings"
@@ -34,6 +35,7 @@ module SensuRunCheck
         statuses = []
         s.get_all_checks.collect{ |check| check[:name] }.sort.each do |checkname|
           stdout, status = s.run_check(checkname)
+          status ||= 3
           statuses << status
           puts "#{checkname} #{status} #{stdout}"
         end
@@ -41,6 +43,7 @@ module SensuRunCheck
       else
         stdout, status = s.run_check(options[:run_check])
         puts stdout
+        status ||= 3
         exit(status)
       end
     end
@@ -55,6 +58,47 @@ module SensuRunCheck
       @settings = Sensu::Settings.get(options)
       @settings.validate
       @logger = SensuRunCheck::NilLog.new
+    end
+
+    # Execute a check command, capturing its output (STDOUT/ERR),
+    # exit status code, execution duration, timestamp, and publish
+    # the result. This method guards against multiple executions for
+    # the same check. Check command tokens are substituted with the
+    # associated client attribute values. If there are unmatched
+    # check command tokens, the check command will not be executed,
+    # instead a check result will be published reporting the
+    # unmatched tokens.
+    #
+    # @param check [Hash]
+    def execute_check_command(check)
+      @logger.debug("attempting to execute check command", :check => check)
+      unless @checks_in_progress.include?(check[:name])
+        @checks_in_progress << check[:name]
+        command, unmatched_tokens = substitute_check_command_tokens(check)
+        if unmatched_tokens.empty?
+          check[:executed] = Time.now.to_i
+          started = Time.now.to_f
+
+          output, status = Open3.capture2(command)
+          check[:duration] = ("%.3f" % (Time.now.to_f - started)).to_f
+          check[:output] = output
+          check[:status] = status.exitstatus
+          @checks_in_progress.delete(check[:name])
+          publish_check_result(check)
+        else
+          check[:output] = "Unmatched command tokens: " + unmatched_tokens.join(", ")
+          check[:status] = 3
+          check[:handle] = false
+          @checks_in_progress.delete(check[:name])
+          publish_check_result(check)
+        end
+      else
+        @logger.warn("previous check command execution in progress", :check => check)
+      end
+    end
+
+    def publish_check_result(check)
+      return check[:output], check[:status]
     end
 
     # Find a Sensu check by name.
@@ -84,16 +128,20 @@ module SensuRunCheck
         execute_check_command(check)
       end
     end
-
   end
 
   # Swallow logs
   class NilLog
-    def debug(*ignore)
-      # Ignore
+    def initialize
+      self.class.create_level_methods
     end
-    def warn(*ignore)
-      # Ignore
+
+    def self.create_level_methods
+      Sensu::Logger::LEVELS.each do |level|
+        define_method(level) do |*args|
+          # Do nothing
+        end
+      end
     end
   end
 end
--- lib/sensu-run-check/version.rb
+++ lib/sensu-run-check/version.rb
@@ -1,3 +1,3 @@
 module SensuRunCheck
-  VERSION = '0.1.0'
+  VERSION = '0.2.0'
 end
